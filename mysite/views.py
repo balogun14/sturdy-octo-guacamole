81ffd653e57a6ccb6bfc648f3a97f96346cc352c
balogun14 <awwalbalogun06@gmail.com>
2023-09-15 21:09:48 +0100

    Added gcbvs

--- mysite/views.py
+++ mysite/views.py
@@ -2,22 +2,33 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 from .models import Post
+from django.views.generic import ListView,DetailView
 
 
-def post_list(request):
-    """
-    docstring
-    """
-    post_list = Post.published.all()
-    paginator = Paginator(post_list,3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, "list.html", {"posts": posts})
+
+class PostListView(ListView):
+    model = Post
+    queryset = Post.published.all()
+    template_name = "list.html"
+    context_object_name = 'posts'
+    paginate_by = 3
+    
+
+# def post_list(request):
+#     """
+#     docstring
+#     """
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list,3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, "list.html", {"posts": posts})
+
 
 
 def post_detail(request,year,month,day,post):
